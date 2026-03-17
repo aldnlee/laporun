33daeb618f50d4d57d7414f2baae0cad5f0fdb09
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 07:15:51 +0700

    awal ngabuburun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    mode: 'directory', // Ini kunci supaya Cloudflare baca folder dist sebagai Pages
-    runtime: { mode: 'complete' } // Supaya semua fitur Node/Worker aktif
-  }),
+  // output: 'static' adalah default, jadi tidak perlu ditulis
   integrations: [tailwind()],
 });
