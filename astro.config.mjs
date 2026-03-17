1e81149ab6b6f2089b4d0a2f9985300aa3c1f6af
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 18:09:54 +0700

    awal ngabuburun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare'; // Ganti dari netlify
+import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server', // Tetap server karena kita pakai Supabase Auth
+  output: 'server',
   adapter: cloudflare({
-    mode: 'directory', // Mode standar untuk Cloudflare Pages
+    mode: 'directory', // Mode ini paling aman buat struktur dist/
   }),
+  // Matikan optimasi gambar Sharp karena Cloudflare gak support
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop' 
+    }
+  },
+  integrations: [tailwind()],
 });
