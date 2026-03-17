5c8e70ea60a40be2318ae40a1cdaa23101dce2a7
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 19:06:04 +0700

    awal ngabuburun

--- public/_worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    // Ini akan memanggil entry point Astro di folder dist
-    const { onRequest } = await import("../dist/server/entry.mjs");
-    return onRequest({ request, env });
-  }
-};
