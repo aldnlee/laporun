47c27bdc2dec4af4b48602925a27e1006e026e32
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 07:25:52 +0700

    Switch to Netlify and add live tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   // output: 'static' adalah default, jadi tidak perlu ditulis
   integrations: [tailwind()],
+
+  adapter: netlify(),
 });
