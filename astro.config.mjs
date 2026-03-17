996e7c527b58e6599eb7341371d5a9b3fdeb4813
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 20:40:17 +0700

    awal ngabuburun

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify({
+    // Tambahkan ini biar Netlify nggak bingung nyari file statis vs dynamic route
+    edgeMiddleware: false 
+  }),
   integrations: [tailwind()],
 });
