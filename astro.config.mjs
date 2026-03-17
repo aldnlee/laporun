7d90aa1536bd51f95657a7d1f4994b451998ce0b
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 23:11:07 +0700

    awal ngabuburun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import zeabur from '@zeabur/astro-adapter';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    // Tambahkan ini biar Netlify nggak bingung nyari file statis vs dynamic route
-    edgeMiddleware: false 
-  }),
+  output: 'server', // Wajib karena kita pakai fitur Tracker/Database
+  adapter: zeabur(),
   integrations: [tailwind()],
 });
