dbe898fa122de9ed4cd9f49a278bc4e368deed11
loq <ALE@LAPTOP-UUM7DVB6>
2026-03-17 18:55:49 +0700

    awal ngabuburun

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    mode: 'advanced',
+    mode: 'directory',
     // Tambahkan ini untuk menghilangkan warning dideprecated
     entrypointResolution: 'auto' 
   }),
